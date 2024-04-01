b62df24e6f5bf52863ab64308a3e54a8a223895f
Paul <2018.tpg@gmail.com>
2024-04-01 14:49:34 +0300

    hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,9 @@ import { LinkMeniu } from "./LinkMeniu";
 export function Navbar() {
     return (
         <div className=" bg-slate-800 flex justify-between p-3">
-            <h1 className=" font-bold text-slate-50">SITE CNSV</h1>
+            <h1 className=" hover:text-red-500 font-bold text-slate-50">
+                SITE CNSV
+            </h1>
 
             <div className=" flex gap-4">
                 <LinkMeniu link="/" text="Home" />
